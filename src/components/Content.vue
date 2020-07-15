32c04e21ecd0a286a1803f79de2cdc13a9b5134e
Sam <Samitone@Sams-MBP.broadband>
2020-07-15 11:48:45 +0100

    Create example using content component.

---
+++ src/components/Content.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="content-main">
+        <h1>Example Content</h1>
+        <img :src="require('../assets/' + img + '')" class="content-image">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Content',
+  props: {
+    img: String
+  },
+}
+</script>
+
+<style scoped>
+
+.content-main {
+    max-width: 500px;
+    margin: auto;
+}
+
+.content-image {
+    max-width: 100%;
+}
+
+</style>
